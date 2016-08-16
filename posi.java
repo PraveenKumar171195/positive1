f08a3cdcde00143c6cb61f13a92ad2185741005a
PraveenKumar171195 <praveenkumarece20@gmail.com>
2016-08-16 18:03:39 +0530

    Update posi.java

--- posi.java
+++ posi.java
@@ -5,7 +5,7 @@ public static void amin(String args[])
 {
 Scanner s=new Scanner(System.in);
 int a=s.nextint();
-if(a>0&&a!=0)
+if(a>0)
 System.out.println("positive");
 else
 System.out.println("Negative");
